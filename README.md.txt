7e9e7b233c6918a79a16db8dc9b30d47201ae439
PatrickRCruz <prichardcruz@gmail.com>
2024-03-03 19:52:09 -0300

    Atualização do projeto com novas funcionalidades.

---
+++ README.md.txt
@@ -0,0 +1,52 @@
+Documentação da Aplicação de Simulação de Recepcção de Hotel
+
+Introdução
+Esta documentação fornece uma visão geral da aplicação de simulação de recepção de hotel, incluindo o front-end e o back-end. 
+A aplicação permite o cadastro, edição e exclusão de hóspedes, além de fornecer funcionalidades de filtragem por nome.
+
+Tecnologias Utilizadas
+
+Front-end
+React.js: Uma biblioteca JavaScript de código aberto para construir interfaces de usuário.
+Axios: Uma biblioteca para fazer requisições HTTP a servidores.
+Bootstrap: Um framework front-end para desenvolvimento ágil de interfaces web.
+
+Back-end
+ASP.NET Core: Um framework de desenvolvimento de software livre e de código aberto desenvolvido pela Microsoft.
+MySQL: Um sistema de gerenciamento de banco de dados relacional de código aberto.
+Estrutura de Arquivos
+
+A estrutura de arquivos da aplicação é organizada da seguinte forma:
+
+
+Front-end
+Funcionalidades Principais
+Cadastro de Novos Hóspedes
+Edição de Hóspedes
+Exclusão de Hóspedes
+Filtragem de Hóspedes por Nome
+Para mais detalhes sobre as funcionalidades do front-end, consulte a documentação específica do front-end.
+
+Back-end
+Funcionalidades Principais
+Obter todos os hóspedes
+Obter hóspede por nome
+Obter hóspede por ID
+Criar um novo hóspede
+Atualizar informações de um hóspede
+Excluir um hóspede
+Para mais detalhes sobre as funcionalidades do back-end, consulte a documentação específica do back-end.
+
+Requisitos
+Node.js instalado na máquina local para executar o front-end.
+ASP.NET Core SDK instalado na máquina local para executar o back-end.
+Conexão com a internet para acessar a API back-end e o banco de dados MySQL.
+Instalação e Execução
+Clone o repositório do projeto para sua máquina local.
+Configure o banco de dados MySQL com as configurações fornecidas no arquivo app.settings.
+No terminal, navegue até o diretório do front-end e execute o comando npm install para instalar todas as dependências.
+Execute o comando npm start para iniciar a aplicação front-end.
+No terminal, navegue até o diretório do back-end e execute o comando dotnet run para iniciar a aplicação back-end.
+Considerações Finais
+Esta documentação fornece uma visão geral das funcionalidades e estrutura da aplicação de simulação de recepção de hotel. 
+Para mais detalhes sobre a implementação específica de cada funcionalidade, consulte o código-fonte fornecido.
--- src/App.js
+++ src/App.js
@@ -1,15 +1,71 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
-import logohotel from './assets/logohotel.jpg';
+import hotelcalifornialogo from './assets/hotelcalifornialogo.jpg';
+
 
 function App() {
 
   const baseUrl = ("https://localhost:44357/api/Guests");
 
   const [data, setData] = useState([]);
+  const [modalIncluir, setModalIncluir] = useState(false);
+  const [modalEditar, setModalEditar] = useState(false);
+  const [modalDelete, setModalDelete] = useState(false);
+  const [updateData, setUpdateData] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const [guestSelect, setguestSelect] = useState({
+    id: '',
+    name: '',
+    email: '',
+    address: '',
+    cpf: '',
+    cellNumber: '',
+    status: ''
+
+  })
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleChangeFilter = e => {
+    const { name, value } = e.target;
+    setguestSelect({
+      ...guestSelect,
+      [name]: value
+    });
+  };
+
+  const selectGuest = (guests, opcao) => {
+    setguestSelect(guests);
+    (opcao === "Editar") ?
+      openCloseModalEditar() : openCloseModalExcluir();
+  }
+
+  const openCloseModalIncluir = () => {
+    setModalIncluir(!modalIncluir);
+  }
+
+  const openCloseModalEditar = () => {
+    setModalEditar(!modalEditar)
+  }
+
+  const openCloseModalExcluir = () => {
+    setModalDelete(!modalDelete)
+  }
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setguestSelect({
+      ...guestSelect, [name]: value
+    });
+    console.log(guestSelect);
+  }
+
 
   const orderGet = async () => {
     await axios.get(baseUrl)
@@ -20,53 +76,191 @@ function App() {
       })
   }
 
-  useEffect(() => {
-    orderGet();
-  })
+  const orderPost = async () => {
+    delete guestSelect.id;
+    await axios.post(baseUrl, guestSelect)
+      .then(response => {
+        setData(data.concat(response.data));
+        setUpdateData(true);
+        openCloseModalIncluir();
+      }).catch(error => {
+        console.log(error);
+      })
+  }
 
+  const orderPut = async () => {
+    await axios.put(baseUrl + "/" + guestSelect.id, guestSelect)
+      .then(response => {
+        var reply = response.data;
+        var auxiliaryData = data;
+        auxiliaryData.map(guests => {
+          if (guests.id === guestSelect.id) {
+            guests.name = reply.name;
+            guests.email = reply.email;
+            guests.address = reply.address;
+            guests.cpf = reply.cpf;
+            guests.cpf = reply.cellNumber;
+            guests.status = reply.status;
+          }
+        });
+        setUpdateData(true);
+        openCloseModalEditar();
+      }).catch(error => {
+        console.log(error);
+      })
+  }
+
+  const orderDelete = async () => {
+    await axios.delete(baseUrl + "/" + guestSelect.id)
+      .then(response => {
+        setData(data.filter(guests => guests.id !== response.data));
+        setUpdateData(true);
+        openCloseModalExcluir();
+      }).catch(error => {
+        console.log(error);
+      })
+
+  }
+
+  useEffect(() => {
+    if (updateData) {
+      orderGet();
+      setUpdateData(false);
+    }
+  }, [updateData])
 
   return (
-    <div className="App">
+    <div className="guest-container">
       <br />
       <h3>Cadastro de Hóspedes</h3>
+          <input
+      type="text"
+      placeholder="Filtrar por nome"
+      value={searchTerm}
+      onChange={handleSearchChange}
+    />
       <header>
-        <img src={''} alt='Cadastro' />
-        <button className='btn btn-success'>Incluir Novo Hóspede </button>
+        <img src={hotelcalifornialogo} alt="Cadastro" />
+        <button className="btn btn-success" onClick={() => openCloseModalIncluir()}> Incluir Novo Hóspede </button>
       </header>
-      <table className='table table-bordered'>
+      <table className="table table-bordered">
         <thead>
           <tr>
             <th>Id</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Adress</th>
-            <th>Cpf</th>
-            <th>CellNumber</th>
-            <th>Checkin</th>
-            <th>Checkout</th>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Endereço</th>
+            <th>CPF</th>
+            <th>Númedo do Celular</th>
+            <th>Operação</th>
           </tr>
         </thead>
         <tbody>
-          {data.map(guest => {
-            <tr key={guest.Id}>
-              <td>{guest.Id}</td>
-              <td>{guest.Name}</td>
-              <td>{guest.Email}</td>
-              <td>{guest.Adress}</td>
-              <td>{guest.Cpf}</td>
-              <td>{guest.CellNumber}</td>
-              <td>{guest.Checkin}</td>
-              <td>{guest.Checkout}</td>
+        {data.filter(guest => guest.name && guest.name.toLowerCase().includes(searchTerm.toLowerCase()))
+  .map(guests => (
+            <tr key={guests.id}>
+              <td>{guests.id}</td>
+              <td>{guests.name}</td>
+              <td>{guests.email}</td>
+              <td>{guests.address}</td>
+              <td>{guests.cpf}</td>
+              <td>{guests.cellNumber}</td>
               <td>
-                <button className="btn btn-primary">Editar</button> {" "}
-                <button className="btn btn-danger">Excluir</button>
+                <button className="btn btn-primary" onClick={() => selectGuest(guests, "Editar")}>Editar</button> {" "}
+                <button className="btn btn-danger" onClick={() => selectGuest(guests, "Exluir")}>Excluir</button>
               </td>
             </tr>
+          ))}
 
-          })}
         </tbody>
-
       </table>
+      <Modal isOpen={modalIncluir}>
+
+        <ModalHeader> Incluir Hóspedes</ModalHeader>
+
+        <ModalBody>
+
+          <div className="form-group">
+            <label>Nome: </label>
+            <br />
+            <input type="text" className="form-control" name="name" onChange={handleChange} />
+            <br />
+            <label>Email:</label>
+            <br />
+            <input type="text" className="form-control" name="email" onChange={handleChange} />
+            <br />
+            <label>Endereço:</label>
+            <br />
+            <input type="text" className="form-control" name="address" onChange={handleChange} />
+            <br />
+            <label>Cpf:</label>
+            <br />
+            <input type="text" className="form-control" name="cpf" onChange={handleChange} />
+            <br />
+            <label>Número do Celular:</label>
+            <br />
+            <input type="text" className="form-control" name="cellNumber" onChange={handleChange} />
+            <br />
+
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-primary" onClick={() => orderPost()}>Registrar Entrada</button>{" "}
+          <button className="btn btn-danger" onClick={() => openCloseModalIncluir()}>Cancelar</button>
+        </ModalFooter>
+
+      </Modal>
+
+      <Modal isOpen={modalEditar}>
+
+        <ModalHeader> Editar Hóspede</ModalHeader>
+
+        <ModalBody>
+
+          <div className="form-group">
+            <label>Id: </label>
+            <br /> <input type="text" className="form-control" readOnly value={guestSelect && guestSelect.id} />
+            <label>Nome: </label><br />
+            <input type="text" className="form-control" name="name" onChange={handleChange}
+              value={guestSelect && guestSelect.name} />
+            <label>Email: </label>
+            <br />
+            <input type="text" className="form-control" name="email" onChange={handleChange}
+              value={guestSelect && guestSelect.email} />
+            <br />
+            <label>Endereço: </label>
+            <br />
+            <input type="text" className="form-control" name="address" onChange={handleChange}
+              value={guestSelect && guestSelect.address} />
+            <br />
+            <label>CPF: </label>
+            <br />
+            <input type="text" className="form-control" name="cpf" onChange={handleChange}
+              value={guestSelect && guestSelect.cpf} />
+            <br />
+            <label>Número do Celular: </label>
+            <br />
+            <input type="text" className="form-control" name="cellNumber" onChange={handleChange}
+              value={guestSelect && guestSelect.cellNumber} />
+            <br />
+
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-primary" onClick={() => orderPut()}>Editar</button>{" "}
+          <button className="btn btn-danger" onClick={() => openCloseModalEditar()}>Cancelar</button>
+        </ModalFooter>
+      </Modal>
+
+      <Modal isOpen={modalDelete}>
+        <ModalBody>
+          Confirma a exlusão deste(a) hóspede(a) : {guestSelect && guestSelect.name} ?
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-danger" onClick={() => orderDelete()}> Sim </button>
+          <button className="btn btn-secundary" onClick={() => openCloseModalExcluir()}> Não </button>
+        </ModalFooter>
+      </Modal>
     </div>
   );
 }
